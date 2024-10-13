dfd8283d3ef751843b4d1cca7193d7a0f6a7c99e
JSamuel <sp4619168@gmail.com>
2024-10-13 15:27:26 -0600

    feat: :lipstick: Crear formulario de cajas
    - Se da estructura inicial a la pagina de administración de cajas registradoras.
    - Se crea el formulario para dar de alta las cajas.

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { CashRegisterAdminComponent } from './pages/cash-register-admin/cash-register-admin.component';
 
 export const AUTH_ROUTES: Routes = [
   // TODO: crear rutas para el login y registro de cajas
   // {path: 'login', component: },
-  // {path: 'register', component: }
+  { path: 'register', component: CashRegisterAdminComponent },
 ];
---
+++ src/app/auth/components/cash-register-table/cash-register-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cash-register-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './cash-register-table.component.html',
+  styleUrl: './cash-register-table.component.css',
+})
+export class CashRegisterTableComponent {}
