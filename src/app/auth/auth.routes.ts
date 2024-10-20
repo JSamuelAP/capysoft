0b8c6a61c441cfb4b985f356226a808af5c48870
RataGalvan <omar_galvan@outlook.es>
2024-10-20 16:44:39 -0600

    feat: se añade interfaz Login

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const AUTH_ROUTES: Routes = [
   // TODO: crear rutas para el login y registro de cajas
   // {path: 'login', component: },
   // {path: 'register', component: }
+  {path:'login', component:LoginPageComponent},
 ];
