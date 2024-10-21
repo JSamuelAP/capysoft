be553f79e236a4f14094fe1ea4f0de1625c631a8
JSamuel <sp4619168@gmail.com>
2024-10-20 21:24:47 -0600

    feat: :sparkles: Mostrar estadisticas en card
    - Se reciben los datos en las propiedades del componente.
    - El componente se renderiza segun el dato a mostrar.

--- src/app/orders/components/estadistica-card/estadistica-card.component.ts
+++ src/app/orders/components/estadistica-card/estadistica-card.component.ts
@@ -1,15 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+
+import { Product } from '../../model/product.interface';
+import { Caja } from '../../model/caja.interface';
 
 @Component({
   selector: 'estadistica-card',
   standalone: true,
-  imports: [CardModule],
+  imports: [CardModule, CommonModule, DividerModule],
   templateUrl: './estadistica-card.component.html',
   styleUrl: './estadistica-card.component.css',
 })
 export class EstadisticaCardComponent {
   @Input()
   title!: string;
+
+  @Input()
+  icon: string = 'pi pi-dollar';
+
+  @Input()
+  moneyAmount?: number;
+
+  @Input()
+  product?: Product;
+
+  @Input()
+  products?: Product[];
+
+  @Input()
+  cajas?: Caja[];
 }
