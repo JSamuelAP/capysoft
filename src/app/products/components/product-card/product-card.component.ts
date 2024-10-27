24a93f8c72c2b32f9d4dd823d73a607363055a85
JSamuel <sp4619168@gmail.com>
2024-10-27 13:57:22 -0600

    feat: :sparkles: Mostrar botones en card productos
    - Boton para seleccionar al generar una orden.
    - Botones para editar y eliminar en el administrador.

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -1,16 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+
 import { Product } from '../../model/product.interface';
 
 @Component({
   selector: 'product-card',
   standalone: true,
-  imports: [CardModule],
+  imports: [ButtonModule, CardModule, CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
   @Input()
   product!: Product;
+
+  @Input()
+  pickable: boolean = false;
+
+  @Input()
+  editable: boolean = false;
+
+  onButtonSelectClick() {
+    // TODO: Cargar producto a la orden
+    console.log(this.product);
+  }
+
+  onButtonEditClick() {
+    // TODO: Cargar datos al formulario
+    console.info(this.product);
+  }
+
+  onButtonDeleteClick() {
+    // TODO: Mostrar mensaje de confirmación
+    console.error(this.product);
+  }
 }
