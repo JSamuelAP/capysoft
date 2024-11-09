4425bb96ed08a90061a1ec0fe42f82ff24e1ac7c
JSamuel <sp4619168@gmail.com>
2024-11-09 13:03:14 -0600

    feat: :lipstick: Quitar navbar del login
    - se crea el componente layout para mostrar el navbar y un container
      para envolver solo las paginas que lo necesiten
    - Ahora las paginas del modulo auth usan la ruta auth/
    - Las rutas no reconocias redirigen al login

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { CashRegisterAdminComponent } from './pages/cash-register-admin/cash-register-admin.component';
-
+import { LayoutComponent } from '../shared/components/layout/layout.component';
 
 export const AUTH_ROUTES: Routes = [
-  // TODO: crear rutas para el login y registro de cajas
-  // {path: 'login', component: },
-  {path:'register', component:CashRegisterAdminComponent},
-  {path:'login', component:LoginPageComponent},
+  {
+    path: 'register',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: CashRegisterAdminComponent,
+      },
+    ],
+  },
+  { path: 'login', component: LoginPageComponent },
 ];
--- src/app/products/products.routes.ts
+++ src/app/products/products.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
 import { ProductsAdminPageComponent } from './pages/products-admin-page/products-admin-page.component';
+import { LayoutComponent } from '../shared/components/layout/layout.component';
 
 export const PRODUCTS_ROUTES: Routes = [
-  // TODO: crear rutas para listar productos y gestionarlos
-  { path: '', component: ProductsPageComponent },
-  { path: 'admin', component: ProductsAdminPageComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: ProductsPageComponent },
+      { path: 'admin', component: ProductsAdminPageComponent },
+    ],
+  },
 ];
