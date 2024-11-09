4dcb9238a3e1094e2959d0900826f28120e1f861
JSamuel <sp4619168@gmail.com>
2024-11-09 13:03:14 -0600

    feat: :lipstick: Quitar navbar del login
    - se crea el componente layout para mostrar el navbar y un container
      para envolver solo las paginas que lo necesiten
    - Ahora las paginas del modulo auth usan la ruta auth/
    - Las rutas no reconocias redirigen al login

--- src/app/orders/orders.routes.ts
+++ src/app/orders/orders.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
+
 import { DashboardVentasComponent } from './pages/dashboard-ventas/dashboard-ventas.component';
+import { LayoutComponent } from '../shared/components/layout/layout.component';
 import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
 
 export const ORDERS_ROUTES: Routes = [
-  { path: '', component: OrdersPageComponent },
-  { path: 'dashboard', component: DashboardVentasComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: OrdersPageComponent },
+      { path: 'dashboard', component: DashboardVentasComponent },
+    ],
+  },
 ];
