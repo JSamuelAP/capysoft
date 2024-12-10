82793c7bdc195a479aee00b1a7dcd7682f3790b7
RataGalvan <omar_galvan@outlook.es>
2024-12-10 01:16:48 -0600

    refactor: se añaden interfaces cabeceraOrden y cuerpoOrden

---
+++ src/app/orders/model/cabeceraOrden.interface.ts
@@ -0,0 +1,10 @@
+import { Time } from "@angular/common";
+
+export interface cabeceraOrden {
+    numOrden : number;
+    usuarios : string;
+    totalPrecio : number;
+    propina : number;
+    fecha : string;
+    hora : string;
+}
